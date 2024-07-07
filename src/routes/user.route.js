e2463be281e215af7f15dd9c4a838a3c7cf3f527
Mukul Anand <mukulanand.biz@gmail.com>
2024-07-08 00:23:59 +0530

    added login route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
@@ -12,5 +12,7 @@ router.post(
     ]),
     registerUser
 );
+
+router.post("/login", loginUser)
 
 export default router
