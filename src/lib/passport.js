bde17c51d9f2e51473ade08fc20c80c26325ab0a
Mukul Anand <mukulanand.biz@gmail.com>
2024-07-10 17:39:18 +0530

    added process.env

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://${BACKEND_URL}/api/v1/users/auth/google/callback",
+      callbackURL: "https://${process.env.BACKEND_URL}/api/v1/users/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
