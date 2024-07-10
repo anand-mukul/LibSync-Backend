78365fdaf37425a647c6e746b757ddcdb2bec1e6
Mukul Anand <mukulanand.biz@gmail.com>
2024-07-10 18:04:13 +0530

    trying completed callback url

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/v1/users/auth/google/callback",
+      callbackURL: "https://api.neosync.cloud/api/v1/users/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
